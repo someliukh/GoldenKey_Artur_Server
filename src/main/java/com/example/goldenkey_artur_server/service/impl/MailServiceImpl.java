95269ae7b479c59ef4ebb7ad50b4426d80d6f38e
StepanOmeliukh <stepan.omeliukh.knm.2019@lpnu.ua>
2024-01-29 21:17:09 +0200

    fixed name and phone number responce

--- src/main/java/com/example/goldenkey_artur_server/service/impl/MailServiceImpl.java
+++ src/main/java/com/example/goldenkey_artur_server/service/impl/MailServiceImpl.java
@@ -4,8 +4,6 @@ import com.example.goldenkey_artur_server.constant.MailConstant;
 import com.example.goldenkey_artur_server.dto.MailDTO;
 import com.example.goldenkey_artur_server.service.MailService;
 import lombok.AllArgsConstructor;
-import lombok.experimental.Accessors;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -18,23 +16,24 @@ public class MailServiceImpl implements MailService {
 
     @Override
     public MailDTO sendMessage(MailDTO mailDTO) {
-        mailDTO.setSubject(mailDTO.getName() + " | " + mailDTO.getPhoneNumber());
-        mailDTO.setMessage("Name: " + mailDTO.getName() + " \n" +
-                "Phone Number: " + mailDTO.getPhoneNumber());
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(MailConstant.SENDER_EMAIL);
-        message.setTo(MailConstant.RECEIVER_EMAIL);
-        message.setSubject(mailDTO.getSubject());
-        message.setText(mailDTO.getMessage());
-        javaMailSender.send(message);
-
-        return MailDTO.builder()
+        MailDTO mail = MailDTO.builder()
                 .sentFrom(MailConstant.SENDER_EMAIL)
                 .sentTo(MailConstant.RECEIVER_EMAIL)
-                .subject(mailDTO.getSubject())
-                .message(mailDTO.getMessage())
+                .subject(mailDTO.getName() + " | " + mailDTO.getPhoneNumber())
+                .message("Name: " + mailDTO.getName() + " \n" +
+                        "Phone Number: " + mailDTO.getPhoneNumber())
+                .name(mailDTO.getName())
+                .phoneNumber(mailDTO.getPhoneNumber())
                 .build();
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(mail.getSentFrom());
+        message.setTo(mail.getSentTo());
+        message.setSubject(mail.getSubject());
+        message.setText(mail.getMessage());
+        javaMailSender.send(message);
+
+        return mail;
     }
 
 
